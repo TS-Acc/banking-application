8ca6a407f526a05328088d9ce8a5ff281f7fb0e8
Trevor Shepherd <t.rayshpd@gmail.com>
2023-02-16 16:00:07 -0500

    Add project files.

---
+++ banking-application/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banking_application
+{
+    internal class Account
+    {
+
+        public string AccountId { get; set; } = string.Empty; // unique id for the account
+        public decimal Balance { get; set; } = 0;
+        public string Description { get; set; } = string.Empty;
+
+        public bool Deposit(decimal Amount)
+        {
+            if (Amount <= 0)
+            {
+                Console.WriteLine($"Amount cannot be zero or negative.");
+                return false; // you can put return on a void method as long as it's not returning
+                        // anything. this will not let the method go to the bottom statement
+            }
+            Balance += Amount; // same as Balance = Balance + Amount;
+            return true;
+        }
+
+        public bool Withdraw(decimal Amount)
+        {
+            if (Amount <= 0)
+            {
+                Console.WriteLine($"Amount cannot be zero or negative.");
+                return false;
+            }
+            if (Amount > Balance)
+            {
+                Console.WriteLine($"Insufficient Funds!");
+                return false;
+            }
+            Balance -= Amount;
+            return true;
+        }
+
+        public bool Transfer(decimal Amount, Account ToAccount)
+        {
+            if (Amount <= 0)
+            {
+                Console.WriteLine($"Amount cannot be zero or negative.");
+                return false;
+            }
+            if(Withdraw(Amount))
+            {
+                ToAccount.Deposit(Amount);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+            //var success = Withdraw(Amount);
+            //if(!success)
+            //{
+            //    return false;
+            //}
---
+++ banking-application/Program.cs
@@ -0,0 +1,21 @@
+﻿using banking_application;
+
+var s1 = new SavingsComp();
+var s2 = new SavingsComp();
+
+s1.AccountId = "Trevor";
+s1.Description = "My savings Account";
+s1.Balance = 1000;
+
+s1.AccountId = "Not Trevor";
+s1.Description = "Not Trevor's savings Account";
+s1.Balance = 1000;
+
+s1.Transfer(100, s2);
+
+
+//Console.WriteLine($"S1 paid interest of {interest:C}");
+
+Console.WriteLine($"The balance of S1 is {s1.Balance:C}"); 
+
+ 
